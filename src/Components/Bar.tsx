23a65b4a7d8e479bffdef9d420b6aba9c28d02b1
kylecarbonell <76983718+kylecarbonell@users.noreply.github.com>
2023-08-24 17:53:42 -0700

    Finishing up nav bar

--- src/Components/Bar.tsx
+++ src/Components/Bar.tsx
@@ -13,6 +13,10 @@ import ContactModal from "./Modal";
 import LoginModal from "./LoginModal";
 import CartImage from "../Images/CartImage.jpg";
 
+import { SideBarData } from "../NavBarData";
+
+// import useWindowDimensions from "../WindowSize";
+
 function Bar() {
   const [cartItems, setCartItems] = useState(0);
   const cartCount = useRef<number>(0);
@@ -50,28 +54,98 @@ function Bar() {
     setLogin(!showLogin);
   };
 
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+
+  function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(
+      getWindowDimensions()
+    );
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+        if (innerWidth > 1400) {
+          setNavbar(false);
+        }
+      }
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowDimensions;
+  }
+
+  const { height, width } = useWindowDimensions();
+
+  const [navbar, setNavbar] = useState(false);
+
   return (
     <>
       <div className="Bar">
+        {navbar && (
+          <div className="NavBar-Container">
+            <div className="ExitButton-Container">
+              <button
+                className="ExitButton"
+                onClick={() => {
+                  setNavbar(false);
+                }}
+              ></button>
+            </div>
+            <div className="NavLink-Container">
+              {SideBarData.map((val, key) => {
+                return (
+                  <Link className="NavLink" to={val.link}>
+                    <div className="NavBarIcon-Container">{val.icon}</div>
+                    <button className="NavBarButton">{val.title}</button>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        )}
+        {width < 1400 && (
+          <div className="NavButton-Container">
+            <button
+              className="NavButton"
+              onClick={() => {
+                setNavbar(!navbar);
+              }}
+            ></button>
+          </div>
+        )}
+
         {/* Add a font to this that matches the whole page */}
         <div className="Links-Container">
-          <nav className="Bar-Container">
-            <Link className="Link" to="/">
-              <button className="Button">Home</button>
-            </Link>
-            <Link className="Link" to="/About">
-              <button className="Button">About</button>
-            </Link>
-          </nav>
+          {width > 1400 && (
+            <nav className="Bar-Container">
+              <Link className="Link" to="/">
+                <button className="Button">Home</button>
+              </Link>
+              <Link className="Link" to="/About">
+                <button className="Button">About</button>
+              </Link>
+            </nav>
+          )}
+
           <h1 className="Title">Jerky Republic</h1>
-          <nav className="Bar-Container">
-            <Link className="Link" to="/Shop">
-              <button className="Button">Shop</button>
-            </Link>
-            <button className="Link Button" onClick={onClickLogin}>
-              Login
-            </button>
-          </nav>
+          {width > 1400 && (
+            <nav className="Bar-Container">
+              <Link className="Link" to="/Shop">
+                <button className="Button">Shop</button>
+              </Link>
+              <button className="Link Button" onClick={onClickLogin}>
+                Login
+              </button>
+            </nav>
+          )}
         </div>
         <Link className="Cart-Link" to="/Cart"></Link>
       </div>
